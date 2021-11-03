095ec7d76966ee7624edb0333fdca4b0c9ec740f
Oleh Potomakha <potomakha@gmail.com>
2021-11-03 19:44:05 +0200

    swap to alphabet

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace lection4
             string[] negativeArray;
             int numPositive = 0;
             int numNegative = 0;
+            string headerSymbol = "A B C D E F G H I J";
 
             Console.Write("Input size array ->");
             sizeArray = Convert.ToInt32(Console.ReadLine());
@@ -22,6 +23,7 @@ namespace lection4
             positiveArray = new string[sizeArray];
             negativeArray = new string[sizeArray];
 
+            // чатсть 1. Разделение на массивы.
             for (int i = 0; i < sizeArray; i++)
             {
                 numericArray[i] = new Random().Next(1, 27);
@@ -40,7 +42,29 @@ namespace lection4
             Array.Resize(ref positiveArray, numPositive);
             Array.Resize(ref negativeArray, numNegative);
 
+            // часть 2. Замена чисел на буквы.
+            SwapToAlphabet(ref positiveArray, headerSymbol);
+            SwapToAlphabet(ref negativeArray, headerSymbol);
+
             Console.ReadKey();
+        }
+
+        private static void SwapToAlphabet(ref string[] inputArray, string headerSymbol)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                int alphabetPosition = Convert.ToInt32(inputArray[i]) - 1;
+                inputArray[i] = alphabet[alphabetPosition].ToString();
+
+                if (headerSymbol.Contains(inputArray[i], StringComparison.OrdinalIgnoreCase))
+                {
+                    inputArray[i] = inputArray[i].ToUpper();
+                }
+            }
+
+            return;
         }
     }
 }
