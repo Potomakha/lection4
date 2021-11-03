493656fb803f994c87c58c6c8716a5b790d28c7a
Oleh Potomakha <potomakha@gmail.com>
2021-11-03 20:10:18 +0200

    header counter. output results

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace lection4
             string[] negativeArray;
             int numPositive = 0;
             int numNegative = 0;
-            string headerSymbol = "A B C D E F G H I J";
+            string headerSymbol = "A D E H I J";
 
             Console.Write("Input size array ->");
             sizeArray = Convert.ToInt32(Console.ReadLine());
@@ -46,7 +46,51 @@ namespace lection4
             SwapToAlphabet(ref positiveArray, headerSymbol);
             SwapToAlphabet(ref negativeArray, headerSymbol);
 
+            // часть 3. Подсчёт заглавных. Вывод результатов.
+            int numPositiveHeader = HeaderCalc(positiveArray, headerSymbol);
+            int numNegativeHeader = HeaderCalc(negativeArray, headerSymbol);
+
+            if (numPositiveHeader > numNegativeHeader)
+            {
+                Console.WriteLine($"Массив с чётной нумерацией имеет больше заглавных букв. Количество -> {numPositiveHeader}");
+            }
+            else if (numPositiveHeader < numNegativeHeader)
+            {
+                Console.WriteLine($"Массив с ytчётной нумерацией имеет больше заглавных букв. Количество -> {numNegativeHeader}");
+            }
+            else
+            {
+                Console.WriteLine($"Массивы имеют равное количество заглавных букв. Количество -> {numNegativeHeader}");
+            }
+
+            Console.WriteLine($"Массив чётных чисел. Заглавные {numPositiveHeader}");
+            foreach (var item in positiveArray)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Массив нечётных чисел. Заглавные {numNegativeHeader}");
+            foreach (var item in negativeArray)
+            {
+                Console.Write(item + " ");
+            }
+
             Console.ReadKey();
+        }
+
+        private static int HeaderCalc(string[] inputArray, string headerSymbol)
+        {
+            int headerCount = 0;
+            foreach (var item in inputArray)
+            {
+                if (headerSymbol.Contains(item))
+                {
+                    headerCount++;
+                }
+            }
+
+            return headerCount;
         }
 
         private static void SwapToAlphabet(ref string[] inputArray, string headerSymbol)
